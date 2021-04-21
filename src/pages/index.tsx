57eb4c03bb78850d3aa05398ef3f66b34df60ce8
Jonatas Amaral <jonataspadtab@gmail.com>
2021-04-21 10:31:53 -0300

    API fetch: SPA strategy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [podData, setPodData] = useState([]);
+  useEffect(()=>{
+    fetch('http://localhost:3333/episodes')
+      .then(response=>response.json())
+      .then(data=>{
+        console.log(data)
+        setPodData(data);
+      })
+  }, [])
+
   return (
-    <h1>index</h1>
+    
+    podData.map(dp=>{
+      return (
+        <>
+          <h2>{dp.title}</h2>
+          <br />
+        </>
+      )
+    })
   )
 }
